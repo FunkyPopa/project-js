1b9b1391db77f2be096a3bda8e4c127e6905ce5f
FunkyPopa <andriybuno370@gmail.com>
2022-07-24 10:56:23 +0300

    Initial commit

---
+++ Lesson 1/main.js
@@ -0,0 +1,96 @@
+// 1
+
+let hello = 'hello'
+console.log(hello)
+alert(hello)
+document.write(hello)
+
+let owu = 'owu'
+console.log(owu)
+alert(owu)
+document.write(owu)
+
+let com = 'com'
+console.log(com)
+alert(com)
+document.write(com)
+
+let ua = 'ua'
+console.log(ua)
+alert(ua)
+document.write(ua)
+
+let n1 = 1
+console.log(n1)
+alert(n1)
+document.write(n1)
+
+let n10 = 10
+console.log(n10)
+alert(n10)
+document.write(n10)
+
+let n999 = -999
+console.log(n999)
+alert(n999)
+document.write(n999)
+
+let n123 = 123
+console.log(n123)
+alert(n123)
+document.write(123)
+
+let n3p14 = 3.14
+console.log(n3p14)
+alert(n3p14)
+document.write(n3p14)
+
+let n2p7 = 2.7
+console.log(n2p7)
+alert(n2p7)
+document.write(n2p7)
+
+let n16 = 16
+console.log(n16)
+alert(n16)
+document.write(n16)
+
+let t = true
+console.log(t)
+alert(t)
+document.write(t)
+
+let f = false
+console.log(f)
+alert(f)
+document.write(f)
+
+// 2
+
+// let firstName = 'Буньо'
+// let middleName = 'Андрій'
+// let lastName = 'Володимирович'
+//
+// let person = firstName + ' ' + middleName + ' ' + lastName
+//
+// console.log(person)
+
+// 3
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+//4
+
+let firstName = prompt("Ваше ім'я?")
+let lastname = prompt("Ваше прізвище?")
+let age = prompt("Ваш вік?")
+
+let result = `Ваше ім'я ${firstName}` + ' ' + `Ваше прізвище ${lastname}` + ' ' + `Ваш вік ${age}`
+
+console.log(result)
