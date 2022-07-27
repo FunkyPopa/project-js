ef5e03053dbcea333383791970b037313f5ba7f9
FunkyPopa <andriybuno370@gmail.com>
2022-07-27 15:26:01 +0300

    add Lesson 2

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -1,69 +1,69 @@
 // 1
 
-let hello = 'hello'
-console.log(hello)
-alert(hello)
-document.write(hello)
-
-let owu = 'owu'
-console.log(owu)
-alert(owu)
-document.write(owu)
-
-let com = 'com'
-console.log(com)
-alert(com)
-document.write(com)
-
-let ua = 'ua'
-console.log(ua)
-alert(ua)
-document.write(ua)
-
-let n1 = 1
-console.log(n1)
-alert(n1)
-document.write(n1)
-
-let n10 = 10
-console.log(n10)
-alert(n10)
-document.write(n10)
-
-let n999 = -999
-console.log(n999)
-alert(n999)
-document.write(n999)
-
-let n123 = 123
-console.log(n123)
-alert(n123)
-document.write(123)
-
-let n3p14 = 3.14
-console.log(n3p14)
-alert(n3p14)
-document.write(n3p14)
-
-let n2p7 = 2.7
-console.log(n2p7)
-alert(n2p7)
-document.write(n2p7)
-
-let n16 = 16
-console.log(n16)
-alert(n16)
-document.write(n16)
-
-let t = true
-console.log(t)
-alert(t)
-document.write(t)
-
-let f = false
-console.log(f)
-alert(f)
-document.write(f)
+// let hello = 'hello'
+// console.log(hello)
+// alert(hello)
+// document.write(hello)
+//
+// let owu = 'owu'
+// console.log(owu)
+// alert(owu)
+// document.write(owu)
+//
+// let com = 'com'
+// console.log(com)
+// alert(com)
+// document.write(com)
+//
+// let ua = 'ua'
+// console.log(ua)
+// alert(ua)
+// document.write(ua)
+//
+// let n1 = 1
+// console.log(n1)
+// alert(n1)
+// document.write(n1)
+//
+// let n10 = 10
+// console.log(n10)
+// alert(n10)
+// document.write(n10)
+//
+// let n999 = -999
+// console.log(n999)
+// alert(n999)
+// document.write(n999)
+//
+// let n123 = 123
+// console.log(n123)
+// alert(n123)
+// document.write(123)
+//
+// let n3p14 = 3.14
+// console.log(n3p14)
+// alert(n3p14)
+// document.write(n3p14)
+//
+// let n2p7 = 2.7
+// console.log(n2p7)
+// alert(n2p7)
+// document.write(n2p7)
+//
+// let n16 = 16
+// console.log(n16)
+// alert(n16)
+// document.write(n16)
+//
+// let t = true
+// console.log(t)
+// alert(t)
+// document.write(t)
+//
+// let f = false
+// console.log(f)
+// alert(f)
+// document.write(f)
 
 // 2
 
@@ -89,8 +89,9 @@ console.log(typeof c)
 
 let firstName = prompt("Ваше ім'я?")
 let lastname = prompt("Ваше прізвище?")
-let age = prompt("Ваш вік?")
+let age = +prompt("Ваш вік?")
 
-let result = `Ваше ім'я ${firstName}` + ' ' + `Ваше прізвище ${lastname}` + ' ' + `Ваш вік ${+age}`
+console.log(firstName)
+console.log(lastname)
+console.log(age)
 
-console.log(result)
---
+++ Lesson 2/main.js
@@ -0,0 +1,70 @@
+//1
+
+let arr = [ 3, 4, 5, 'apple', 'orange', true, 3.5, false, 9, 343];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+//2-3
+
+let book1 = {
+    title: 'Cafe at the edge of the world.',
+    pageCount: 128,
+    genre: 'Modern prose',
+    autor: ['Streleki J.', 52]
+};
+
+console.log(book1);
+
+let book2 = {
+    title: 'Witcher: the last desire.',
+    pageCount: '288',
+    genre: 'Novel',
+    autor: ['Andrzej Sapkowski', 74]
+    };
+
+console.log(book2);
+
+let book3 = {
+    title: 'Ghost of Kiev',
+    pageCount: 16,
+    genre: 'Visual novel',
+    autor: ['Matsuda Hi', 40]
+    };
+
+console.log(book3);
+
+//4
+
+let users = [
+    user1 = {name: 'Andrew', username: 'FunkyPotato', password: 's11134dv'},
+    user2 = {name: 'Sergey', username: 'GrayHead', password: 'hJq49901W'},
+    user3 = {name: 'Marina', username: 'Mary', password: 'jjres268hQ2'},
+    user4 = {name: 'Misha', username: 'mishanya_08', password: '408jhQDlr2r'},
+    user5 = {name: 'Volodimir', username: 'Vovan_777', password: 'jgdsf5343u'},
+    user6 = {name: 'Galina', username: 'Galinka', password: 'jkruiol3256U'},
+    user7 = {name: 'Mariana', username: 'Nyashka', password: 'tyIo2256yT'},
+    user8 = {name: 'Igor', username: '_I4_', password: 'nnF3356tR'},
+    user9 = {name: 'Vasil', username: 'Vasya', password: 'bbQf3470okL'},
+    user10 = {name: 'Maxim', username: 'maXXXon', password: 'fsdg373YY77'}
+];
+ console.log(users);
+
+console.log(users[0]["password"]);
+console.log(users[1]["password"]);
+console.log(users[2]["password"]);
+console.log(users[3]["password"]);
+console.log(users[4]["password"]);
+console.log(users[5]["password"]);
+console.log(users[6]["password"]);
+console.log(users[7]["password"]);
+console.log(users[8]["password"]);
+console.log(users[9]["password"]);
