31c05f8023f1e1fdd530f74a810ee8318a6e8667
FunkyPopa <andriybuno370@gmail.com>
2022-08-03 19:52:12 +0300

    fixed some issues

--- Lesson 4/main.js
+++ Lesson 4/main.js
@@ -26,13 +26,14 @@
 //     i++
 // }
 
-//5 !!!!!! я не зрозумів як зробити, щоб всі li обгорталися лише в 1 ul, а не обгорталися в окремі
+//5
 
 // let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-//
+//     document.write(`<ul>`);
 // for (const item of listOfItems) {
 //     document.write(`<li>${item}</li>`);
 // }
+//     document.write(`</ul>`);
 
 //6
 
