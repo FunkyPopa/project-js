ef3692b305f1183689a8d90233968347b4e334cf
FunkyPopa <andriybuno370@gmail.com>
2022-08-08 21:35:10 +0300

    fixed 10 and 11 tasks

--- Lesson 5/main.js
+++ Lesson 5/main.js
@@ -114,30 +114,32 @@ objectList(objects);
 
 //10
 
-let numbers1 = [3,5,8];
+let numbers1 = [5,3,8,12,1,34];
 
-function minNumber (arr) {
-    if (arr[0] < arr[1] && arr[0] < arr[2]) {
-        console.log(arr[0]);
-    } else if (arr[1] < arr[0] && arr[1] < arr[2]) {
-        console.log(arr[1])
-    } else if (arr[2] < arr[0] && arr[2] < arr[1]) {
-        console.log(arr[2])
-    } else {
-        console.log('error');
-    }
+const minNumber = (arr) => {
+    let min = arr[0]
+        for (let item of arr) {
+            if (min > item) {
+                min = item
+            }
+        }
+    return min;
 }
 
-minNumber(numbers1);
+console.log(minNumber(numbers1));
 
 
 //11
 
 let numbers2 = [10,20,30,60];
 
-function sum (arr) {
-    const numCounter = arr[0] + arr[1] + arr[2] + arr[3];
-    return numCounter;
+const foo = (arr) => {
+    let sum = 0;
+    for (let item of arr) {
+        sum += item
+    }
+    return sum;
 }
 
-console.log(sum(numbers2));
+
+console.log(foo(numbers2));
